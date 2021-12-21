8d6ce4ee531ceaed94d043f9527735b367d6343f
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2021-12-21 21:31:49 +0000

    Integrated ejs renderer
    - needed for data binding

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import bodyParser from 'body-parser';
 import module from 'path';
 
 
+
 const __dirname = module.resolve();
 const app = express();
 const PORT = 4000;
 
 app.use(bodyParser.json());
+
 
 app.use('/css', express.static(module.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(module.join(__dirname, 'node_modules/bootstrap/dist/js')))
@@ -17,8 +19,17 @@ app.use('/js', express.static(module.join(__dirname, 'node_modules/jquery/dist')
 app.listen(PORT, () => {
     console.log(`Servers running on port http://localhost:${PORT}`);
 })
+app.set('view engine', 'ejs');
 
+// use res.render to load up an ejs view file
+
+// index page
 app.get('/', function(req, res) {
-    res.sendFile(module.join(__dirname + '/views/index.html'));
-    //__dirname : It will resolve to your project folder.
+
+    var tagline = "Ali";
+
+    res.render('index', {
+
+        tagline: tagline
+    });
 });
