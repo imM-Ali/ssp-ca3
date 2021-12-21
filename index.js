5c703469a22f929d00e1b1d0557343f327d6ecc2
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2021-12-21 20:20:50 +0000

    Installed path and bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import module from 'path';
 
+
+const __dirname = module.resolve();
 const app = express();
 const PORT = 4000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT ,() =>{
-console.log(`Servers running on port http://localhost/${PORT}`);
+app.use('/css', express.static(module.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(module.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(module.join(__dirname, 'node_modules/jquery/dist')))
+
+
+app.listen(PORT, () => {
+    console.log(`Servers running on port http://localhost:${PORT}`);
 })
+
+app.get('/', function(req, res) {
+    res.sendFile(module.join(__dirname + '/views/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
